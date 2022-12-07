dd1b1f1d1c3d4ad990ae00618a3c981e16eb3e67
Student User <student@workstation.lab.example.com>
2022-12-07 13:15:44 -0500

    index.php change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n" . "Srinadh Delta";
+print "Hello, World! php version is " . PHP_VERSION . "\n" . "Srinadh Delta" . "\n\n" . "WEB CONSOLE" . "\n";
 ?>
